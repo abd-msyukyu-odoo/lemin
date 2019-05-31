4fbfc178c7a8275f1d319433b0f3504829ae2508
Damien Abeloos <dabeloos@w-r9-p1.s19.be>
2019-05-31 21:46:19 +0200

    bellmanford not well implemented

--- python/bellmanford.py
+++ python/bellmanford.py
@@ -13,16 +13,12 @@ class BellmanFord:
 	def solve(self):
 		for room in self.rooms:
 			self.paths.add_data(Path(room, None))
-		for room in self.rooms:
-			path = self.paths.get_data(room.name)
-			for jroom in room.get_joint_rooms():
-				if jroom is self.s_room:
-					continue
-				tunnel = room.get_tunnel(jroom)
-				jpath = self.paths.get_data(jroom.name)
-				if jpath.previous is None or jpath.cost > tunnel.cost + path.cost:
-					jpath.cost = tunnel.cost + path.cost
-					jpath.previous = path
+		for i in range(len(self.rooms) - 1):
+			self.__sub_solve(i)
+		i = len(self.rooms) - 1
+		if self.__sub_solve(i):
+			print("error")
+			return
 		path = self.paths.get_data(self.e_room.name)
 		if path is not None:
 			reverse_path = Path(path.room, None)
@@ -30,3 +26,18 @@ class BellmanFord:
 				path = path.previous
 				reverse_path = Path(path.room, reverse_path)
 			self.start_path = reverse_path
+
+	def __sub_solve(self, i):
+		changed = False
+		room = self.rooms[i]
+		path = self.paths.get_data(room.name)
+		for jroom in room.get_joint_rooms():
+			if jroom is self.s_room:
+				continue
+			tunnel = room.get_tunnel(jroom)
+			jpath = self.paths.get_data(jroom.name)
+			if jpath.previous is None or jpath.cost > tunnel.cost + path.cost:
+				jpath.cost = tunnel.cost + path.cost
+				jpath.previous = path
+				changed = True
+		return changed
