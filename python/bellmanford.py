c1d29c4a1386e4cb13e1def60e55b80884d86e8b
msyukyu <damien_ab@hotmail.com>
2019-06-01 20:00:42 +0200

    bellman ford : correction (negative cycle check still missing)

--- python/bellmanford.py
+++ python/bellmanford.py
@@ -15,16 +15,21 @@ class BellmanFord:
 			self.paths.add_data(Path(room, None))
 		paths = [self.paths.get_data(self.s_room.name)]
 		for i in range(len(self.rooms) - 1):
+			new_paths = []
 			for path in paths:
-				new_paths = []
-
-			#pour chaque connexion de chaque room de chaque path a la distance courante
-				#evaluer si le passage par ce tunnel reduit le cout d'acces a
-				#cette room
-
-		#old
-		for i in range(len(self.rooms)):
-			self.__sub_solve(i)
+				room = path.room
+				for jroom in room.get_joint_rooms():
+					if jroom is self.s_room:
+						continue
+					tunnel = room.get_tunnel(jroom)
+					jpath = self.paths.get_data(jroom.name)
+					if jpath.previous is None or jpath.cost > tunnel.cost + path.cost:
+						jpath.cost = tunnel.cost + path.cost
+						jpath.previous = path
+						new_paths.append(jpath)
+			paths = new_paths
+			if len(paths) == 0:
+				break
 		path = self.paths.get_data(self.e_room.name)
 		if path is not None:
 			reverse_path = Path(path.room, None)
@@ -32,18 +37,4 @@ class BellmanFord:
 				path = path.previous
 				reverse_path = Path(path.room, reverse_path)
 			self.start_path = reverse_path
-
-	def __sub_solve(self, i):
-		changed = False
-		room = self.rooms[i]
-		path = self.paths.get_data(room.name)
-		for jroom in room.get_joint_rooms():
-			if jroom is self.s_room:
-				continue
-			tunnel = room.get_tunnel(jroom)
-			jpath = self.paths.get_data(jroom.name)
-			if jpath.previous is None or jpath.cost > tunnel.cost + path.cost:
-				jpath.cost = tunnel.cost + path.cost
-				jpath.previous = path
-				changed = True
-		return changed
+		# missing : check if negative cycle (useless ????)
