fb31987eac295a066542195b53d29a3ec35edf06
Damien Abeloos <dabeloos@w-r9-p2.s19.be>
2019-06-30 17:24:01 +0200

    many small mallocs vs one big malloc

---
+++ tests/_maximalloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "room.h"
+#include "tube.h"
+#include "array.h"
+#include "btree.h"
+#include <time.h>
+
+//gcc -o maximalloc _maximalloc.c ../globals/room.c ../globals/tube.c ../globals/array.c ../globals/btree.c -I../includes -I../libft -L../libft/ -lft
+
+int					main(void)
+{
+	static void		*ptr;
+	clock_t			t1;
+	clock_t			t2;
+
+	t1 = clock();
+	ptr = malloc(1000000 * sizeof(t_room));
+	t2 = clock();
+	printf("%f\n", (float)(t2 - t1) / CLOCKS_PER_SEC);
+	t1 = clock();
+	free(ptr);
+	t2 = clock();
+	printf("%f\n", (float)(t2 - t1) / CLOCKS_PER_SEC);
+	return (0);
+}
---
+++ tests/_minimalloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "room.h"
+#include "tube.h"
+#include "array.h"
+#include "btree.h"
+#include <time.h>
+
+//gcc -o minimalloc _minimalloc.c ../globals/room.c ../globals/tube.c ../globals/array.c ../globals/btree.c -I../includes -I../libft -L../libft/ -lft
+
+int					main(void)
+{
+	static void		*ptr[1000000];
+	clock_t			t1;
+	clock_t			t2;
+
+	t1 = clock();
+	for (int i = 0; i < 1000000; i++)
+		ptr[i] = malloc(sizeof(t_room));
+	t2 = clock();
+	printf("%f\n", (float)(t2 - t1) / CLOCKS_PER_SEC);
+	t1 = clock();
+	for (int i = 0; i < 1000000; i++)
+		free(ptr[i]);
+	t2 = clock();
+	printf("%f\n", (float)(t2 - t1) / CLOCKS_PER_SEC);
+	return (0);
+}
