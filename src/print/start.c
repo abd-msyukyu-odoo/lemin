bb0ffa8ac092638445f63bd2ffb976c2ad0a0b3b
Pierre Vanderlynden <pvanderl@w-r2-p5.s19.be>
2019-07-11 15:51:20 +0200

    first push

--- src/print/start.c
+++ src/print/start.c
@@ -12,35 +12,6 @@
 
 #include "lemin.h"
 
-void	print_btree(t_btree *t, char *s) {
-	if (t)
-	{
-		printf("%s-%s", s, t->data->key);
-		print_btree(t->right, s);
-		print_btree(t->left, s);
-	}
-}
-
-void	print_tmp(t_global *s) {
-	printf("NB Ants: %d\n", s->nb_ants);
-	if (s->start)
-	{
-		printf("start room name: %s\n", s->start->key.key);
-		print_btree(s->start->bt_tubes, s->start->key.key);
-	}
-	else
-		printf("no start room\n");
-	if (s->end)
-		printf("end room name: %s\n", s->end->key.key);
-	else
-		printf("no end room\n");
-	if (s->bt_rooms)
-	{
-
-	}
-
-}
-
 //TODO Gestion d'erreur
 // "ERROR" si pas assez de données mais quoi si malloc error?
 
