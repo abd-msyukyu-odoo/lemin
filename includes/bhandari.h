b7064f7070e2432c092cb6e7a926237c20972ddd
Damien Abeloos <dabeloos@w-r10-p2.s19.be>
2019-08-11 12:29:55 +0200

    c algo

---
+++ includes/bhandari.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bhandari.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/11 12:23:25 by dabeloos          #+#    #+#             */
+/*   Updated: 2019/08/11 12:23:27 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BHANDARI_H
+# define BHANDARI_H
+# include <stdlib.h>
+# include "data.h"
+# include "btree.h"
+# include "array.h"
+# include "libft.h"
+
+/*
+**
+*/
+typedef struct			s_bdari
+{
+	
+}						t_bdari;
+
+#endif
