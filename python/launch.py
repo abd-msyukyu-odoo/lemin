50c0f01cc490665ffd6e74362413f3576d388ea7
pierre <pierre.vanderlynden@gmail.com>
2020-01-08 10:52:21 +0100

    little python change

--- python/launch.py
+++ python/launch.py
@@ -17,8 +17,8 @@ o = os.path.join(parent, "resources", "result.txt")
 
 n = 500
 Tools.verbose = False
-visual = False
-c_test = True
+visual = True
+c_test = False
 python_test = False
 
 if system() == 'Darwin':
@@ -27,74 +27,80 @@ else:
 	make = "mingw32-make.exe"
 
 os.system(" ".join([make, "-C", parent, "re"]))
-fails = 0
-results = []
-requireds = []
-for i in range(n):
-	if system() == 'Darwin':
-		k = open(f, 'w')
-		d = os.path.join(parent, "resources", "generator")
-		input_file = os.popen(d + " " + sys.argv[1]).read()
-		k.write(input_file)
-		k.close()
-	else:
-		k = open(f, 'r')
-		input_file = k.read()
-		k.close()
-	sleep(0.25)
-	rtree, nbAnts, Tools.start_name, Tools.end_name, required = read_input(input_file)
 
-	if python_test:
-		bhandari = Bhandari(rtree.get_data(Tools.start_name), rtree.get_data(Tools.end_name), rtree, nbAnts)
-	#print(str(i))
-	if n == 1 and visual and python_test:
-		config = Config()
+# si visu
+if len(sys.argv) == 1 or sys.argv[1] == "visu":
 
-		visu = Floor([bhandari.s_room])
-		visu.draw()
-
-		cg = ColorGenerator(bhandari.pathCostDistribution.paths)
-		for path in bhandari.pathCostDistribution.paths:
-			path.draw(cg.color(path.cost))
-
-	if not c_test and python_test:
-		print("python result : " + str(bhandari.pathCostDistribution.cost) + "/" + str(required) + (" FAIL" if bhandari.pathCostDistribution.cost > required else ""))
-	else:
+# si tests
+else :
+	fails = 0
+	results = []
+	requireds = []
+	for i in range(n):
 		if system() == 'Darwin':
-			prog = "../lem-in"
+			k = open(f, 'w')
+			d = os.path.join(parent, "resources", "generator")
+			input_file = os.popen(d + " " + sys.argv[1]).read()
+			k.write(input_file)
+			k.close()
 		else:
-			prog = "..\\lem-in.exe"
-		os.system(" ".join([prog, "<", f, ">", o]))
+			k = open(f, 'r')
+			input_file = k.read()
+			k.close()
 		sleep(0.25)
-		k = open(o, 'r')
-		output = k.read()
-		k.close()
-		output = output.splitlines()
-		j = 0
-		for line in output:
-			j += 1
-			if line == "":
-				j = len(output) - j
-				break
+		rtree, nbAnts, Tools.start_name, Tools.end_name, required = read_input(input_file)
+
 		if python_test:
-			if j > bhandari.pathCostDistribution.cost or bhandari.pathCostDistribution.cost > required:
-				fails += 1
-				requireds.append(required)
-				results.append(j)
-			print("result c/python/required : " + str(j) + "/" + str(bhandari.pathCostDistribution.cost) + "/" + str(required) + (" FAIL" if j > bhandari.pathCostDistribution.cost or bhandari.pathCostDistribution.cost > required else ""))
+			bhandari = Bhandari(rtree.get_data(Tools.start_name), rtree.get_data(Tools.end_name), rtree, nbAnts)
+		#print(str(i))
+		if n == 1 and visual and python_test:
+			config = Config()
+
+			visu = Floor([bhandari.s_room])
+			visu.draw()
+
+			cg = ColorGenerator(bhandari.pathCostDistribution.paths)
+			for path in bhandari.pathCostDistribution.paths:
+				path.draw(cg.color(path.cost))
+
+		if not c_test and python_test:
+			print("python result : " + str(bhandari.pathCostDistribution.cost) + "/" + str(required) + (" FAIL" if bhandari.pathCostDistribution.cost > required else ""))
 		else:
-			if j > required:
-				fails += 1
-				requireds.append(required)
-				results.append(j)
+			if system() == 'Darwin':
+				prog = "../lem-in"
+			else:
+				prog = "..\\lem-in.exe"
+			os.system(" ".join([prog, "<", f, ">", o]))
+			sleep(0.25)
+			k = open(o, 'r')
+			output = k.read()
+			k.close()
+			output = output.splitlines()
+			j = 0
+			for line in output:
+				j += 1
+				if line == "":
+					j = len(output) - j
+					break
+			if python_test:
+				if j > bhandari.pathCostDistribution.cost or bhandari.pathCostDistribution.cost > required:
+					fails += 1
+					requireds.append(required)
+					results.append(j)
+				print("result c/python/required : " + str(j) + "/" + str(bhandari.pathCostDistribution.cost) + "/" + str(required) + (" FAIL" if j > bhandari.pathCostDistribution.cost or bhandari.pathCostDistribution.cost > required else ""))
+			else:
+				if j > required:
+					fails += 1
+					requireds.append(required)
+					results.append(j)
 
-	if n == 1 and visual and python_test:
-		ants = DisplayAnts(bhandari.pathCostDistribution.ants_distribution, bhandari.pathCostDistribution.paths)
+		if n == 1 and visual and python_test:
+			ants = DisplayAnts(bhandari.pathCostDistribution.ants_distribution, bhandari.pathCostDistribution.paths)
 
-print(str(fails) + " fails sur " + str(n) + " essais")
-print("results: " + str(results))
-print("requireds: " + str(requireds))
-results = np.array(results)
-requireds = np.array(requireds)
-b = results - requireds
-print(sys.argv[1] + " DONE")
+	print(str(fails) + " fails sur " + str(n) + " essais")
+	print("results: " + str(results))
+	print("requireds: " + str(requireds))
+	results = np.array(results)
+	requireds = np.array(requireds)
+	b = results - requireds
+	print(sys.argv[1] + " DONE")
